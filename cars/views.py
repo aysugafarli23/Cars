bb2de9fc3f20e50cb9b2e640f488a0da5457f583
Aysu Gafarli <aysuqafarli02@gmail.com>
2024-06-05 11:44:56 +0400

    Added views

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Car, Owner, Rentacar
 
 # Create your views here.
+
+def general__view(request):
+    cars = Car.objects.all()
+    owners = Owner.objects.all()
+    rentacars = Rentacar.objects.all()
+    
+    context = {
+        "cars": cars,
+        "owners": owners,
+        "rentacars": rentacars
+    }
+    
+    return render(request, 'index.html', context)
